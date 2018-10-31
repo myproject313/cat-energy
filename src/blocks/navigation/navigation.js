0963d7b6081b0a2c2e0cf5d67147336f10e0fabd
Dmitriy Kuznetsov <git.hubb@bk.ru>
2018-10-31 16:38:57 +0300

    Изменяет скрипт, добавляет поддержку no-js для навигации

--- src/blocks/navigation/navigation.js
+++ src/blocks/navigation/navigation.js
@@ -1,11 +1,10 @@
 (function () {
+    let noJs = document.querySelector("body");
     let navigationButton = document.querySelector(".navigation__button");
-    let navigationBlockMenu = document.querySelector(".navigation__list");
-    navigationButton.classList.remove("navigation__button_hidden");
-    navigationBlockMenu.classList.add("navigation__list_hidden");
+    let navigationSection = document.querySelector(".navigation");
+    noJs.classList.remove("no-js");
     navigationButton.addEventListener("click", (ev) => {
         ev.preventDefault();
-        navigationButton.classList.toggle("opened");
-        navigationBlockMenu.classList.toggle("navigation__list_hidden");
+        navigationSection.classList.toggle("opened");
     });
 })();
